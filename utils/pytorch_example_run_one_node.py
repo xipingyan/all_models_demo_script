74f5f7fd4fcd6b0dfc7bfa825c9de9c74ea4a35a
xipingya <xiping.yan@intel.com>
2023-05-09 00:53:10 +0000

    Add run 1 node for pytorch

---
+++ utils/pytorch_example_run_one_node.py
@@ -0,0 +1,15 @@
+# Refer: https://pytorch.org/docs/stable/generated/torch.Tensor.scatter_.html#torch.Tensor.scatter_
+# Dependencies:
+# $ pip install torch
+
+import torch
+src = torch.arange(1, 11).reshape((2, 5))
+index = torch.tensor([[0, 1, 2, 0]])
+dst = torch.zeros(3, 5, dtype=src.dtype)
+
+print("ndim = ", 0)
+print("src=\n", src)
+print("index=\n", index)
+print("src dst=\n", dst)
+dst.scatter_(0, index, src)
+print("final dst=\n", dst)
