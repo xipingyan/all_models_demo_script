8b8b6a1c7be243154d2adfd3a23b6d1cff63202f
xipingya <xiping.yan@intel.com>
2025-01-17 17:03:53 +0800

    add yolov6 py script

--- yolo/run_yolov6.sh
+++ yolo/run_yolov6.sh
@@ -1,4 +1,6 @@
+source /opt/intel/oneapi/setvars.sh
 source python-env/bin/activate
 source ../../openvino/build/install/setupvars.sh
 
-python3 yolov6_ov.py
+OV_GPU_DumpLayersRaw=1 OV_GPU_DumpLayersPath=./output_binary/ 
+OV_GPU_Verbose=4 python3 yolov6_ov.py
--- yolo/yolov6_ov.py
+++ yolo/yolov6_ov.py
@@ -1,4 +1,33 @@
 import openvino as ov
-core = ov.Core()
+import cv2
+import numpy as np
 
-core.
+model_path = './yolov6_customer_model/yolov6s_int8.xml'
+
+img = cv2.imread("dss1.png")
+img = cv2.resize(img, (1920, 1088))
+img = np.asarray(img)
+input_tensor = np.expand_dims(img, 0)
+
+print("input_tensor.shape = ", input_tensor.shape)
+
+core = ov.Core()
+model = core.read_model(model_path)
+
+ppp = ov.preprocess.PrePostProcessor(model)
+ppp.input().tensor().set_element_type(ov.Type.f16).set_layout(ov.Layout('NHWC'))  # noqa: N400
+
+# 2) Here we suppose model has 'NCHW' layout for input
+ppp.input().model().set_layout(ov.Layout('NHWC'))
+
+# 3) Set output tensor information:
+# - precision of tensor is supposed to be 'f32'
+ppp.output().tensor().set_element_type(ov.Type.f32)
+
+# 4) Apply preprocessing modifing the original 'model'
+model = ppp.build()
+
+compiled_model = core.compile_model(model, "GPU")
+results = compiled_model.infer_new_request({0: input_tensor})
+
+print("results=", results)
